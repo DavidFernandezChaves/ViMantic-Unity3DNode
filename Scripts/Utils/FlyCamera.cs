49c8dc1d7c16e6fe478ad161338fe419420c3f63
DavidFernandezChaves <davfercha@uma.es>
2021-06-16 17:49:49 +0200

    2.0.4

--- Scripts/Utils/FlyCamera.cs
+++ Scripts/Utils/FlyCamera.cs
@@ -14,8 +14,8 @@ public class FlyCamera : MonoBehaviour
     space : Moves camera on X and Z axis only.  So camera doesn't gain any height*/
 
 
-    public float mainSpeed = 100.0f; //regular speed
-    public float shiftAdd = 250.0f; //multiplied by how long shift is held.  Basically running
+    public float mainSpeed = 1; //regular speed
+    public float shiftAdd = 2; //multiplied by how long shift is held.  Basically running
     public float maxShift = 1000.0f; //Maximum speed when holdin gshift
     public float camSens = 0.25f; //How sensitive it with mouse
     private Vector3 lastMouse = new Vector3(255, 255, 255); //kind of in the middle of the screen, rather than at the top (play)
