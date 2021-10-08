49c0de19a02420285e67a89faa1f7e38361b8c67
David Fernández Chaves <davfercha@uma.es>
2021-10-08 19:35:41 +0200

    2.2.0

--- Scripts/Utils/FlyCamera.cs
+++ Scripts/Utils/FlyCamera.cs
@@ -3,100 +3,86 @@ using System.Collections;
 
 public class FlyCamera : MonoBehaviour
 {
-
-    /*
-    Writen by Windexglow 11-13-10.  Use it, edit it, steal it I don't care.  
-    Converted to C# 27-02-13 - no credit wanted.
-    Simple flycam I made, since I couldn't find any others made public.  
-    Made simple to use (drag and drop, done) for regular keyboard layout  
-    wasd : basic movement
-    shift : Makes camera accelerate
-    space : Moves camera on X and Z axis only.  So camera doesn't gain any height*/
-
-
-    public float mainSpeed = 1; //regular speed
-    public float shiftAdd = 2; //multiplied by how long shift is held.  Basically running
-    public float maxShift = 1000.0f; //Maximum speed when holdin gshift
-    public float camSens = 0.25f; //How sensitive it with mouse
-    private Vector3 lastMouse = new Vector3(255, 255, 255); //kind of in the middle of the screen, rather than at the top (play)
-    private float totalRun = 1.0f;
+    [TextArea]
+    public string MyTextArea ="Instrucciones: \n A-Left \n D -Right";
+    public float moveSpeed = 1;
+    public float rotationSpeed = 20;
+    
     void Update()
     {
-        if (Input.GetMouseButton(1))
-        {
-            // Mouse input.
-            lastMouse = Input.mousePosition - lastMouse;
-            lastMouse = new Vector3(-lastMouse.y * camSens, lastMouse.x * camSens, 0);
-            lastMouse = new Vector3(transform.eulerAngles.x + lastMouse.x, transform.eulerAngles.y + lastMouse.y, 0);
-            transform.eulerAngles = lastMouse;
-            
-        }
-        lastMouse = Input.mousePosition;
-
         // Keyboard commands.
-        Vector3 p = getDirection();
-        if (Input.GetKey(KeyCode.LeftShift))
-        {
-            totalRun += Time.deltaTime;
-            p = p * totalRun * shiftAdd;
-            p.x = Mathf.Clamp(p.x, -maxShift, maxShift);
-            p.y = Mathf.Clamp(p.y, -maxShift, maxShift);
-            p.z = Mathf.Clamp(p.z, -maxShift, maxShift);
-        }
-        else
-        {
-            totalRun = Mathf.Clamp(totalRun * 0.5f, 1f, 1000f);
-            p = p * mainSpeed;
-        }
+        Vector3 traslation = getDirection();
+        traslation *= Time.deltaTime * moveSpeed;
+        transform.Translate(traslation);
 
-        p = p * Time.deltaTime;
+        Vector3 eulerAngles = transform.rotation.eulerAngles + getRotation()* rotationSpeed * Time.deltaTime;
+        transform.rotation = Quaternion.Euler(eulerAngles);
+
         Vector3 newPosition = transform.position;
         if (Input.GetKey(KeyCode.V))
         { //If player wants to move on X and Z axis only
-            transform.Translate(p);
+            transform.Translate(traslation);
             newPosition.x = transform.position.x;
             newPosition.z = transform.position.z;
             transform.position = newPosition;
         }
         else
         {
-            transform.Translate(p);
+            transform.Translate(traslation);
         }
 
     }
 
     private Vector3 getDirection()
     {
-        Vector3 p_Velocity = new Vector3();
+        Vector3 direction = new Vector3();
         if (Input.GetKey(KeyCode.W))
         {
-            p_Velocity += new Vector3(0, 0, 1);
+            direction += new Vector3(1, 0, 0);
         }
         if (Input.GetKey(KeyCode.S))
         {
-            p_Velocity += new Vector3(0, 0, -1);
+            direction += new Vector3(-1, 0, 0);
         }
         if (Input.GetKey(KeyCode.A))
         {
-            p_Velocity += new Vector3(-1, 0, 0);
+            direction += new Vector3(0, 0, 1);
         }
         if (Input.GetKey(KeyCode.D))
         {
-            p_Velocity += new Vector3(1, 0, 0);
-        }
-        if (Input.GetKey(KeyCode.E))
-        {
-            p_Velocity += new Vector3(0, 1, 0);
+            direction += new Vector3(0, 0, -1);
         }
         if (Input.GetKey(KeyCode.C))
         {
-            p_Velocity += new Vector3(0, -1, 0);
+            direction += new Vector3(0, -1, 0);
         }
-        return p_Velocity;
+        if (Input.GetKey(KeyCode.Space))
+        {
+            direction += new Vector3(0, 1, 0);
+        }
+        return direction;
     }
 
-    public void resetRotation(Vector3 lookAt)
+    private Vector3 getRotation()
     {
-        transform.LookAt(lookAt);
+        Vector3 rotation = new Vector2();
+        if (Input.GetKey(KeyCode.Q))
+        {
+            rotation += new Vector3(0,-1,0);
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            rotation += new Vector3(0, 1, 0);
+        }
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            rotation += new Vector3(0, 0, -1);
+        }
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            rotation += new Vector3(0, 0, 1);
+        }
+
+        return rotation;
     }
 }
